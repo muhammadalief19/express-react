4e4543ea43c81561d89a6f5c64f3fc4b6da83809
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-10-17 14:56:42 +0700

    instalasi dan konfigurasi reactjs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Routing from "./template/Routing";
+
+function App() {
+  return (
+    <>
+      <div className="w-full">
+        <Routing />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/template/Routing.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Mahasiswa from "../pages/Mahasiswa";
+import Jurusan from "../pages/Jurusan";
+export default function Routing() {
+  return (
+    <>
+      <Router>
+        <div className="navbar bg-base-100 px-3">
+          <div className="flex-1">
+            <Link to={"/"} className="btn btn-ghost normal-case text-xl">
+              daisyUI
+            </Link>
+          </div>
+          <div className="flex-none gap-2">
+            <div className="form-control">
+              <ul className=" w-max flex gap-5">
+                <li className="">
+                  <Link to={"/mhs"}>Mahasiswa</Link>
+                </li>
+                <li className="">
+                  <Link to={"/jurusan"}>Jurusan</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <Routes>
+          <Route path="/mhs" element={<Mahasiswa />} />
+          <Route path="/jurusan" element={<Jurusan />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
